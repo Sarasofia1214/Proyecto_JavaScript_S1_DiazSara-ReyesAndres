15fafa23ea30da00c8c3a4ce8a0444090d480600
Sarasofia1214 <dasaro1206@gmail.com>
2025-05-20 15:10:51 -0500

    feat: :sparkles: Creating the admin index

--- script/script.js
+++ script/script.js
@@ -1,14 +1,15 @@
-document.getElementById('login_containerr').addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const user = document.getElementById('input_user').value;
-  const password = document.getElementById('input_password').value;
-
-  if (user === 'a123' && password === 'a123') {
-      window.location.href = '../index/inicio.html';  
-  } else if (user === 'u123' && password === 'u123') {
-      window.location.href = '../index/pilotos.html';  
-  } else {
-      alert('Usuario o contraseña incorrectos');
-  }
-});
+document.getElementById('login_form').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const user = document.getElementById('input_user').value;
+    const password = document.getElementById('input_password').value;
+  
+    if (user === 'Gaia' && password === 'Gaia123') {
+      window.location.href = '../index/inicio.html';
+    } else if (user === 'Malu' && password === 'Malu123') {
+      window.location.href = '../index/inicio_admin.html';
+    } else {
+      console.log('Usuario o contraseña incorrectos');
+    }
+  });
+  
