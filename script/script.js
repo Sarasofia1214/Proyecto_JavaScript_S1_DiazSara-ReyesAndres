19dce00e2ba6afff96db3fd15ceb5e87cdec03f4
Sarasofia1214 <dasaro1206@gmail.com>
2025-05-20 15:56:46 -0500

    fix: :sparkles: Fixing the root

--- script/script.js
+++ script/script.js
@@ -5,9 +5,9 @@ document.getElementById('login_form').addEventListener('submit', function (e) {
     const password = document.getElementById('input_password').value;
   
     if (user === 'Admi' && password === 'Admi123') {
-      window.location.href = '/index/inicio_admi.html';
+      window.location.href = './index/inicio_admi.html';
     } else if (user === 'User' && password === 'User123') {
-      window.location.href = '/index/inicio.html';
+      window.location.href = './index/inicio.html';
     } else {
       console.log('Usuario o contraseña incorrectos');
     }
