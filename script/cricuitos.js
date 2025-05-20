1b04862e0ffb1d72fd24cd182efd0aac87cc39b9
andres8073562 <espineldavid56@gmail.com>
2025-05-19 19:43:18 -0500

    feat sparkles: Adding the escuderias menu

--- script/cricuitos.js
+++ script/cricuitos.js
@@ -1,9 +1,2 @@
-function mostrarEscuderias() {
-    console.log("holi")
-    document.getElementById("cuadro").style.display = "flex"
-    ;
-}
-
-function ocultarEscuderias() {
-    document.getElementById("cuadro").style.display = "none";
-}
+function mostrarCircuitos() {
+        
