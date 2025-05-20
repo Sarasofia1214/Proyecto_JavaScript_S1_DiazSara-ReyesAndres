d6a4d35fc7cdd26ca0a542f0f70ece3689980b6d
Sarasofia1214 <dasaro1206@gmail.com>
2025-05-20 09:14:53 -0500

    feat: :sparkles: adding the login functions

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
-window.addEventListener('load', () => {
-  const sound = document.getElementById('engine-sound');
-  sound.play().catch(e => {
-    console.log("El sonido no se pudo reproducir automáticamente:", e);
-  });
+document.getElementById('login_containerr').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const user = document.getElementById('input_user').value;
+  const password = document.getElementById('input_password').value;
+
+  if (user === 'a123' && password === 'a123') {
+      window.location.href = '../index/inicio.html';  
+  } else if (user === 'u123' && password === 'u123') {
+      window.location.href = '../index/pilotos.html';  
+  } else {
+      alert('Usuario o contraseña incorrectos');
+  }
 });
