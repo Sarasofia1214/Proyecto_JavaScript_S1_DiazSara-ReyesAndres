6dde3e80789cb7c4714d9dab585a45dbf649f1e4
andres8073562 <espineldavid56@gmail.com>
2025-05-20 09:06:03 -0500

    feat: :sparkles: SCRIPT ESCUDERIAS Y CIRCUITOS

--- script/cricuitos.js
+++ script/cricuitos.js
@@ -1,2 +1,10 @@
-function mostrarCircuitos() {
+function mostrarCircuitos(){
+
+    document.getElementById("principal").style.display = "flex";
+
+}
+
+function ocultarCircuitos() {
+    document.getElementById("principal").style.display = "none"
+}
         
--- script/escuderias.js
+++ script/escuderias.js
@@ -1,5 +1,43 @@
+
+async function fetchEquipos() {
+  let response = await fetch("https://682a64c2ab2b5004cb3698d4.mockapi.io/formulaone/formula")
+  let data = await response.json()
+  console.log(data)
+  renderEquipos(data)
+}
+
+function renderEquipos(parametro) {
+  const template = document.getElementById("template");
+  const container = document.getElementById("container_escuderia");
+  
+  const escuderia = parametro[0]["equipos"]
+
+
+
+ escuderia.forEach(escuderia => {
+      const clone = template.cloneNode(true);
+      clone.style.display = "flex"
+      clone.querySelector(".nombre_1").textContent = escuderia ["nombreCompleto"];
+      clone.querySelector(".pais_1").textContent = escuderia ["pais"];
+      clone.querySelector(".modelo_1").textContent = escuderia ["modelo"];
+      clone.querySelector(".entrada_1").textContent = escuderia ["entradaEquipo"]
+  
+  
+  
+  });
+}
+
+fetchEquipos()
+
+
+
+
+
+
+
+
 function mostrarEscuderias() {
-    console.log("holi")
+    
     document.getElementById("cuadro").style.display = "flex"
     ;
 }
