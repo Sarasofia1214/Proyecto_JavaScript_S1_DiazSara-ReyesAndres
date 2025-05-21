71f0943e1577cd6971e4589077011512c2df4c94
andres8073562 <espineldavid56@gmail.com>
2025-05-20 20:41:26 -0500

    feat: :fire: modified escuderias

--- script/escuderias.js
+++ script/escuderias.js
@@ -3,28 +3,25 @@ async function fetchEquipos() {
   let response = await fetch("https://682a64c2ab2b5004cb3698d4.mockapi.io/formulaone/formula")
   let data = await response.json()
   console.log(data)
-  renderEquipos(data)
+  renderEquipos(data) // solo pasamos el array de escuderías
+  
 }
 
 function renderEquipos(parametro) {
   const template = document.getElementById("template");
-  const container = document.getElementById("container_escuderia");
-  
-  const escuderia = parametro[0]["equipos"]
+  const container = document.getElementById("cuadrado");
+  const equipos = parametro[0]["equipos"]
 
 
-
- escuderia.forEach(escuderia => {
+  equipos.forEach(equipo=> {
       const clone = template.cloneNode(true);
       clone.style.display = "flex"
-      clone.querySelector(".nombre_1").textContent = escuderia ["nombreCompleto"];
-      clone.querySelector(".pais_1").textContent = escuderia ["pais"];
-      clone.querySelector(".modelo_1").textContent = escuderia ["modelo"];
-      clone.querySelector(".entrada_1").textContent = escuderia ["entradaEquipo"]
-  
-  
+      clone.querySelector(".nombre_1").textContent = equipo.nombreCompleto;
+      container.appendChild(clone);
+     console.log(equipo.nombreCompleto);
   
   });
+  
 }
 
 fetchEquipos()
