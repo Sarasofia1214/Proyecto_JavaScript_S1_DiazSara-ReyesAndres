70788b674c3e609fdaac3d6f400ebdb818993d09
Sarasofia1214 <dasaro1206@gmail.com>
2025-05-23 06:44:05 -0500

    fix: :ambulance: Fixing the login

--- script/script.js
+++ script/script.js
@@ -1,16 +1,14 @@
 document.getElementById('login_form').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    const user = document.getElementById('input_user').value;
-    const password = document.getElementById('input_password').value;
-  
-    if (user === 'Admi' && password === 'Admi123') {
-      window.location.href = "./indexAdmi/inicio_admi.html";
-    } else if (user === 'User' && password === 'User123') {
-      window.location.href = "./index/inicio.html";
-    } else {
-      console.log('Usuario o contraseña incorrectos');
-    }
-  });
+  e.preventDefault();
 
-  
+  const user = document.getElementById('input_user').value;
+  const password = document.getElementById('input_password').value;
+
+  if (user === 'Admi' && password === 'Admi123') {
+    window.location.href = "./indexAdmi/inicio_admi.html";
+  } else if (user === 'User' && password === 'User123') {
+    window.location.href = "./index/inicio.html";
+  } else {
+    console.log('Usuario o contraseña incorrectos');
+  }
+});
